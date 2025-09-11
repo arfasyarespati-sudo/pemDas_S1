f0c5ee67efe5664ae5b7b3504989f847e443e81f
Muhammad Arfasya <arfasyarespati@gmail.com>
2025-09-11 14:30:06 +0700

    Celcius to Fahrenheit Converter

---
+++ CelFah.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CelFah {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan suhu dalam Celcius! ");
+        int celcius = input.nextInt();
+
+        input.close();
+
+        System.out.println("Konversi suhu celcius ke fahrenheit adalah = " + (celcius * 9 / 5 + 32));
+
+    }
+
+}
