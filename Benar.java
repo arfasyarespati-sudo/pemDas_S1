a4a9773058f1d163c5842a7656ef71e3a8822671
Muhammad Arfasya <arfasyarespati@gmail.com>
2025-09-14 23:20:21 +0700

    Tugas_pemDas_S1

--- Benar.java
+++ Benar.java
@@ -1,24 +1,27 @@
+import java.util.Scanner;
+
 public class Benar {
-
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("pagii!");
+        System.out.println("sebutkan satu makanan! ");
+        String makanan = input.nextLine();
+
+        switch (makanan) {
+            case "Pizza":
+                System.out.println("makan Pizza");
+                break;
+            case "Ayam":
+                System.out.println("makan Ayam");
+                break;
+            case "Ketupat":
+                System.out.println("makan Ketupat");
+            default:
+                System.out.println("apaansih");
+
+                input.close();
+
+        }
 
     }
-
 }
-
-// System.out.println("========= List Barang =========");
-// System.out.printf("%-20s: %02d-%02d-%d\n", "Tanggal", tanggalBeli + "-" +
-// bulanBeli + "-" + tahunBeli);
-// System.out.printf("%20s: %s\n", "Barang Belanja : " + belanjaBarang);
-// System.out.printf("%-20s: %d\n", "Jumlah Barang : " + jumlahBarang);
-// System.out.printf("%-20s: %,.2f\n", "Harga Barang : " + hargaBarang);
-// System.out.printf("%-20s: %s\n", "Diskon : " + diskon);
-// System.out.println("-------------------------------");
-// System.out.printf("%-20s %,.2f\n", "Total Harga : " + hargaBarang *
-// jumlahBarang);
-// System.out.printf("%-20s: %,.2f\n",
-// "Pembayaran : " + ((hargaBarang * jumlahBarang - (hargaBarang * jumlahBarang
-// * diskon / 100))));
-// System.out.printf("%-20s: %s\n", "Pembayaran tunai: " + jenisPembayaran);
--- CelFah.java
+++ CelFah.java
@@ -4,7 +4,7 @@ public class CelFah {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan suhu dalam Celcius! ");
+        System.out.print("Masukkan suhu dalam Celcius= ");
         int celcius = input.nextInt();
 
         input.close();
