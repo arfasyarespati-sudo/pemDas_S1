46371aed3277ad9bc71d985ccdd698ad463d695d
Muhammad Arfasya <arfasyarespati@gmail.com>
2025-09-29 23:22:57 +0700

    belajar

---
+++ Array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner oawihuyr = new Scanner(System.in);
+
+        String euygywa8h[] = { "soeihfiueosrg", "pewergewret", "pqieurdf", "eufygrduf" };
+        System.out.println(euygywa8h[0]);
+        System.out.println(euygywa8h[1]);
+        System.out.println(euygywa8h[2]);
+        System.out.println(euygywa8h[3]);
+        System.out.println(euygywa8h.length);
+
+        oawihuyr.close();
+    }
+
+}
