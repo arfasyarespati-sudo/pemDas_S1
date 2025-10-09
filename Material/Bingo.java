5582b4f258d853f5e07858ee67da31d50cedaa46
Muhammad Arfasya <arfasyarespati@gmail.com>
2025-10-09 22:23:31 +0700

    belajar

---
+++ Material/Bingo.java
@@ -0,0 +1,14 @@
+package Material;
+
+import java.util.Scanner;
+
+public class Bingo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+   
+        
+
+
+        
+    }
+}
--- SecretCode.java
+++ SecretCode.java
@@ -1,23 +1,23 @@
-
 import java.util.Scanner;
 
 public class SecretCode {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("input id ");
-        int id = input.nextInt();
-        int n = input.nextInt(); // length
-        int[] arr = new int[n];
 
-        for (int i = 0; i < n; i++) {
+        System.out.println("Masukkan id");
+        int id = input.nextInt();
+        System.out.println("Banyak");
+        int banyak = input.nextInt();
+        int[] arr = new int[banyak];
+
+        for (int i = 0; i < banyak; i++) {
             arr[i] = input.nextInt();
         }
 
         StringBuilder pesan = new StringBuilder();
         StringBuilder salah = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int numba = arr[i];
 
+        for (int numba : arr) {
             if (numba >= id && numba <= id + 25) {
                 char huruf = (char) ('A' + (numba - id));
                 pesan.append(huruf);
@@ -26,16 +26,21 @@ public class SecretCode {
                 pesan.append(" ");
 
             } else {
-                salah.append(numba).append(", ");
+
+                if (salah.length() > 0) {
+                    salah.append(", ");
+
+                }
+                salah.append(numba);
+
             }
         }
 
-        System.out.println(pesan.toString());
-        if (salah.length() > 0) {
+        System.out.println("Output pesan: " + pesan.toString());
+        if (salah.length() > 0)
             System.out.println("Elemen tidak valid ditemukan: " + salah.toString().trim());
-        }
 
         input.close();
-    }
 
+    }
 }
