4cbf70eb1a39e1dd0f06d5adfed98ab29529c7b6
Muhammad Arfasya <arfasyarespati@gmail.com>
2025-12-02 23:22:57 +0700

    belajar

--- Material/Arrays.java
+++ Material/Arrays.java
@@ -6,24 +6,24 @@ public class Arrays {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String[] brg = { "A12", "B45", "C20", "A35", "D78" };
+        String[] brgs = { "A12", "B45", "C20", "A35", "D78" };
 
         System.out.print("Masukkan kode barang: ");
         String put = input.nextLine();
-        String[] arrput = put.split(" ");
-        boolean ditemukan = false;
+        String[] arrputs = put.split(" ");
 
-        for (String space : arrput) {
-
-            for (String barang : brg) {
-                if (space.equals(barang)) {
+        for (String arrput : arrputs) {
+            boolean ditemukan = false;
+            for (String brg : brgs) {
+                if (arrput.equals(brg)) {
                     ditemukan = true;
                     break;
                 }
+
                 if (ditemukan) {
-                    System.out.println(barang + " : " + "Kode ditemukan.");
+                    System.out.println(brg + " : " + "Kode ditemukan.");
                 } else {
-                    System.out.println(barang + " : Kode tidak ditemukan.");
+                    System.out.println(brg + " : Kode tidak ditemukan.");
                 }
             }
         }
